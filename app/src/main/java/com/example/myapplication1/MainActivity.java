5b76f03d0c6ed9b54659eb5aba40267376a5b5b8
Dosmamatov044 <dosmamatov24@gmail.com>
2020-03-29 22:47:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,184 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity  {
+
+    TextView result;
+    Double firstValues, secondValues, result_op;
+    String operation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        result = findViewById(R.id.result_field);
+    }
+
+    public void onNumberClick(View view) throws Exception {
+
+        try {
+
+
+            switch (view.getId()) {
+
+                case R.id.zero:
+
+                    result.append("0");
+
+
+                    break;
+
+                case R.id.one:
+                    result.append("1");
+                    break;
+
+                case R.id.two:
+                    result.append("2");
+                    break;
+
+                case R.id.three:
+                    result.append("3");
+                    break;
+
+                case R.id.four:
+                    result.append("4");
+                    break;
+                case R.id.five:
+                    result.append("5");
+                    break;
+
+
+                case R.id.six:
+                    result.append("6");
+                    break;
+                case R.id.seven:
+                    result.append("7");
+                    break;
+                case R.id.eight:
+                    result.append("8");
+                    break;
+                case R.id.nine:
+                    result.append("9");
+                    break;
+
+                case R.id.clear:
+                    result.setText("");
+                    break;
+                case R.id.dot:
+
+
+                    String string = (result.getText().toString()).trim();
+                    if (string.length() > 0) {
+                        result.setText(string + " . ");
+
+                        break;
+                    }
+
+                case R.id.comma:
+
+
+                    String string2 = (result.getText().toString()).trim();
+                    if (string2.length() > 0) {
+                        result.setText(string2 + " , ");
+
+                        break;
+
+                    }
+                    break;
+
+            }
+        } catch (Exception e) {
+        }
+    }
+    public void onOperationClick(View view)  {
+        try {
+
+
+            switch (view.getId()) {
+                case R.id.minus:
+                    operation = "-";
+                    firstValues = Double.valueOf(result.getText().toString());
+                    result.setText(firstValues + "-");
+                    break;
+                case R.id.multiply:
+                    operation = "*";
+                    firstValues = Double.valueOf(result.getText().toString());
+                    result.setText(firstValues + "*");
+                    break;
+                case R.id.plus:
+                    operation = "+";
+                    firstValues = Double.valueOf(result.getText().toString());
+                    result.setText(firstValues + "+");
+                    break;
+                case R.id.divide:
+                    operation = "/";
+                    firstValues = Double.valueOf(result.getText().toString());
+                    result.setText(firstValues + "/");
+              //  case R.id.moduloDivision:
+               //     operation = "%";
+               //     firstValues = Double.valueOf(result.getText().toString());
+                //    result.setText(firstValues + "%");
+
+
+                case R.id.equal:
+                    if (!operation.equals(null)) {
+                        String two = result.getText().toString().replace(firstValues.toString() + operation, "");
+                        secondValues = Double.valueOf(two);
+                        if (operation == "+") {
+                            result_op = firstValues + secondValues;
+                            result.setText(result_op.toString());
+                            operation = "+";
+                        }
+
+                        if (operation == "-") {
+                            result_op = firstValues - secondValues;
+                            result.setText(result_op.toString());
+                            operation = "-";
+                        }
+
+
+                        if (operation == "*") {
+                            result_op = firstValues * secondValues;
+                            result.setText(result_op.toString());
+                            operation = "*";
+                        }
+                        if (operation == "/") {
+                            result_op = firstValues / secondValues;
+                            result.setText(result_op.toString());
+                            operation = "/";
+
+                            //if (operation == "%") {
+                              //  result_op = firstValues % secondValues; не работает
+                               // result.setText(result_op.toString());
+                               // operation = "%";
+
+
+                                break;
+                           // }
+
+
+                        }
+                    }
+            }
+
+
+        }catch(Exception c){
+
+
+        }
+        }
+
+
+    }
+
+
+
+
+
+
+
